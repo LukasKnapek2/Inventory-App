f3e023bdb119f6ea29228fc64f1274b5267c2d41
JuergenManfred <everest3221@gmail.com>
2026-04-18 11:06:06 +0200

    feat: update navbar, add styles

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "defaultsecret",
